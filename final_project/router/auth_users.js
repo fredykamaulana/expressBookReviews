aaebadaa437b12256ce1d3ae4dd15de77dc9d428
fredykamaulana <dyka.maulana@gmail.com>
2024-11-03 23:20:24 -0500

    feat: completing authenticated user service

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -57,7 +57,7 @@ regd_users.post("/login", (req,res) => {
     req.session.authorization = {
         accessToken, username
     }
-    return res.status(200).send("User successfully logged in");
+    return res.status(200).json({ message: "Success Logged in" });
   } else {
     return res.status(208).json({ message: "Invalid Login. Check username and password" });
   }
@@ -66,7 +66,22 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const username = req.session.authorization[username]
+  let book = books[isbn]
+
+  if(book) {
+    let review = book.bookReview
+    review[username] = req.query.review
+    //review.username = req.query.review
+
+    books[isbn].bookReview = review
+
+    return res.status(200).json({ message: "Your review submitted" });
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+  
 });
 
 module.exports.authenticated = regd_users;
